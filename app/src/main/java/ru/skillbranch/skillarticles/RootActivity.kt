db0d231916e8ca49e417fd04af6245fe305e7d94
Bkulesh <kbob@inbox.ru>
2019-12-21 17:49:12 +1000

    first

--- app/src/main/java/ru/skillbranch/skillarticles/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/RootActivity.kt
@@ -3,7 +3,7 @@ package ru.skillbranch.skillarticles
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
