f1d362072879def02499af459582fde0441b7f27
Bkulesh <kbob@inbox.ru>
2020-03-06 00:46:42 +1000

    hometask_4 24.02.2020 all lesson , all local tests passed,n1 test passed, +res to git

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -2,8 +2,7 @@ package ru.skillbranch.skillarticles.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.
-preference.PreferenceManager
+import androidx.preference.PreferenceManager
 
 class PrefManager(context: Context){
     internal val preferences: SharedPreferences by lazy { PreferenceManager(context).sharedPreferences }
