8603b186d8dbb4e2ba99e2a19b5cd3719fa86fcb
Bkulesh <kbob@inbox.ru>
2020-03-09 00:27:26 +1000

    hometask_5 1:04:47

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -5,7 +5,7 @@ import java.util.regex.Pattern
 object MarkdownParser {
     private val LINE_SEPARATOR=System.getProperty("line.separator") ?: "\n"
 
-    private const val UNORDERED_LIST_ITEM_GROUP = ""
+    private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+$)"
 
     const val MARKDOWN_GROUPS="$UNORDERED_LIST_ITEM_GROUP"
 
@@ -27,13 +27,38 @@ object MarkdownParser {
         val matcher = elementsPatten.matcher(string)
         var lastStartIndex=0
 
-        while (matcher.find(lastStartIndex)) {
+        loop@while (matcher.find(lastStartIndex)) {
             val startIndex=matcher.start()
             val endIndex=matcher.end()
 
             if(lastStartIndex<startIndex){
                 parents.add(Element.Text(string.subSequence(lastStartIndex,startIndex)))
             }
+
+            var text: CharSequence
+
+            val groups=1..1
+            var group=-1
+            for(gr in groups){
+                if(matcher.group(gr)!=null){
+                    group=gr
+                    break
+                }
+            }
+
+            when(group) {
+                -1 -> break@loop
+                1->{
+                    text=string.subSequence(startIndex.plus(2),endIndex)
+
+                    val subs= findElements(text)
+                    val element=Element.UnorderedListItem(text,subs)
+                    parents.add(element)
+
+                    lastStartIndex=endIndex
+                }
+            }
+
         }
 
         return parents
