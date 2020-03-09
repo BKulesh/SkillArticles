9bd0cbc6cf70ce3b15f3f009b9907efd30854602
BKulesh <52197610+BKulesh@users.noreply.github.com>
2020-03-09 15:37:41 +1000

    hometask_5 before pull

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -1,12 +1,14 @@
 package ru.skillbranch.skillarticles.data.local
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 
+//@SuppressLint("RestrictedApi")
 class PrefManager(context: Context){
-    internal val preferences: SharedPreferences by lazy { PreferenceManager(context).sharedPreferences }
+    internal val preferences: SharedPreferences by lazy  { PreferenceManager(context).sharedPreferences }
 
-    var storedBoolean by PrefDelegate(false)
+    //var storedBoolean by PrefDelegate(false)
 
 }
